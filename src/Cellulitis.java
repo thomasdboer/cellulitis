c606c967a075e81df00c7a83fc62353dd1383394
ThoompyEagle <thoompyeagle@gmail.com>
2016-09-26 13:08:29 +0200

    added code

--- src/Cellulitis.java
+++ src/Cellulitis.java
@@ -1,8 +1,11 @@
 /**
  * Cellulitis
  * by Thomas Willems
- * and Thomas den Boer
+ * and Thomas den Boer hoi
  */
+
+import java.util.*;
+
 
 public class Cellulitis {
     boolean[] currentGeneration;
@@ -10,6 +13,35 @@ public class Cellulitis {
     public void readGeneral() {
         //TODO read the general input (up to the initial configuration)
         
+        Scanner sc = new Scanner ( System.in );
+        String rule = sc.next();
+        int l = sc.nextInt();
+        int g = sc.nextInt();
+        
+        if (rule.equals ("A")) {
+            
+        } 
+        else if (rule.equals("B")) {
+            
+        } else {
+            System.out.println("Invalid ruleset");
+            System.exit(0);
+        }
+        
+        if (l<0) {
+            System.out.println("Invalid value for L");
+            System.exit(0);
+        }
+        
+        if (g<0) {
+            System.out.println("Invalid value for G");
+            System.exit(0);
+        }
+        
+        
+
+
+
 
 
         //END TODO
@@ -18,6 +50,25 @@ public class Cellulitis {
 
     public void readInitial() {
         //TODO read the initial configuration (build the first currentGeneration)
+        Scanner sc = new Scanner ( System.in );
+
+        if ( sc.next().equals("init_start")) {
+            int t = 0;
+            int[] a = new int[t];
+            
+            if (sc.hasNextInt() == true) {
+                for (int i = 0; i<t; i++) {
+                    a[i] = sc.nextInt();
+                    t++;
+                }
+          
+                        
+            }
+            
+            System.out.println(a);
+        }
+       
+         
 
 
 
@@ -34,6 +85,9 @@ public class Cellulitis {
     boolean newCellValueByA(int k) {
         // TODO return the value {true, false} of cell number k
         // for the next generation according to the rules of A
+        
+        
+        
 
         return false;
 
@@ -43,6 +97,8 @@ public class Cellulitis {
     boolean newCellValueByB(int k) {
         // TODO return the value {true, false} of cell number k
         // for the next generation according to the rules of B
+        
+        
 
         return false;
 
@@ -78,6 +134,8 @@ public class Cellulitis {
 
     public void run() {
         //TODO implement the procedure for the assignment
+        readGeneral();
+        
 
 
 
@@ -93,4 +151,3 @@ public class Cellulitis {
         (new Cellulitis()).run();
     }
 }
-0
